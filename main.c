435e6758595090b9ba17ceb3b13bba505c808015
acidburn <cc.acidburn@gmail.com>
2023-06-11 21:50:47 +0200

    iterative binary search vs recursive

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 // Binary search algorithm allows to find an element in a sorted array, much quicker than in an unsorted array.
+// Recursive binary search
 int binary_search(int arr[], int el, int lIndex, int rIndex) {
    if (lIndex > rIndex) {
 	  return -1;
@@ -19,12 +20,29 @@ int binary_search(int arr[], int el, int lIndex, int rIndex) {
    return 0;
 }
 
+// Iterative binary search
+int binary_search_iterative(int arr[], int el, int lIndex, int rIndex) {
+   while (lIndex <= rIndex) {
+	  int midPoint = (rIndex - lIndex) / 2 + lIndex;
+	  
+	  if (arr[midPoint] == el) {
+		 return el;
+	  } else if (arr[midPoint] > el) {
+		 rIndex = midPoint - 1;
+	  } else {
+		 lIndex = midPoint + 1;
+	  }
+   }
+   
+   return -1;
+}
+
 int main()
 {
    int sorted[] = {1,2,3,4,5,6,7,8,9,10,11,12,13};
    
    int rIndex = sizeof(sorted) / sizeof(sorted[0]);
-   int result = binary_search(sorted, 12, 8, rIndex);
+   int result = binary_search_iterative(sorted, 12, 8, rIndex);
    printf("Index of 12 is: %d", result);
    
    return 0;
